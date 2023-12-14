2e6befa662f9f699e1a5558507e4831a175cda48
Javier1609 <jvrgmz@myuax.com>
2023-12-14 12:38:47 +0100

    .

--- ejercicio_c++.cpp
+++ ejercicio_c++.cpp
@@ -12,7 +12,27 @@ struct Estudiante {
     string nombre;
     int edad;
     float promedio;
-};
+    vector<Materia> materias;
+}
+
+void agregarMateria(Estudiante& estudiante, const std::string& nombre, float calificacion) {
+    estudiante.materias.push_back({nombre, calificacion});
+}
+
+void eliminarMateria(Estudiante& estudiante, const std::string& nombre) {
+    estudiante.materias.erase(
+            std::remove_if(estudiante.materias.begin(), estudiante.materias.end(),
+                           [nombre](const Materia& materia) { return materia.nombre == nombre; }),
+            estudiante.materias.end());
+}
+
+void mostrarMaterias(const Estudiante& estudiante) {
+    std::cout << "Materias Inscritas:" << std::endl;
+    for (const auto& materia : estudiante.materias) {
+        std::cout << "- " << materia.nombre << " (Calificación: " << materia.calificacion << ")" << std::endl;
+    }
+}
+
 
 void mostrarEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << endl;
